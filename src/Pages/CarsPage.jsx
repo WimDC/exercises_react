27c2c1582831b310b1b1c9590243f8d875044f28
WimDC <r0894616@student.thomasmore.be>
2023-03-12 16:41:34 +0100

    3.3 car color -> klaar

--- src/Pages/CarsPage.jsx
+++ src/Pages/CarsPage.jsx
@@ -1,6 +1,17 @@
 import { Cars } from '../components/Cars'
 
 export const CarsPage = ({cars, title}) => {
+const colorLookupTable = {
+  "rood": "red",
+  "blauw": "blue",
+  "geel": "yellow",
+  "paars": "purple",
+  "groen": "green",
+  "grijs": "gray",
+  "zwart": "black",
+}
+let carColor;
+
     return (
         <div id= "main div" style={{backgroundColor: "lightblue", height: 750}}>
             <h2>{title}</h2>
@@ -8,7 +19,7 @@ export const CarsPage = ({cars, title}) => {
                 <h3>{car.name}</h3>
                 {car.brand && <p>merk: {car.brand}</p>}
                 {car.type && <p>type: {car.type}</p>}
-                {car.color && <p>kleur: {car.color}</p>}
+                {car.color && <p style={{backgroundColor: carColor = colorLookupTable[car.color], color: (carColor == "black"? "white" : "black")}}>kleur: {car.color}</p>}
                 {car.note && <p>note: {car.note}</p>}
             </div>)}
         </div>
